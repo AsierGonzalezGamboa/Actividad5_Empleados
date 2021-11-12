244f903df2463706dc62702ec51bf4d61f93f4cf
Asier Gonzalez <agonzalgam1@educacion.navarra.es>
2021-11-12 11:02:48 +0100

    Clase Empleado creada

---
+++ Empleados.php
@@ -0,0 +1,58 @@
+<?php
+    abstract class Empleados
+    {
+        private $nombre;
+        private $apellido;
+        private $numeroSeguridadSocial;
+
+        public function __construct($nombre, $apellido, $numeroSeguridadSocial)
+        {
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+        }
+        
+        public function getNombre()
+        {
+                return $this->nombre;
+        }
+
+        public function setNombre($nombre)
+        {
+                $this->nombre = $nombre;
+
+                return $this;
+        }
+
+        public function getApellido()
+        {
+                return $this->apellido;
+        }
+
+        public function setApellido($apellido)
+        {
+                $this->apellido = $apellido;
+
+                return $this;
+        }
+
+        public function getNumeroSeguridadSocial()
+        {
+                return $this->numeroSeguridadSocial;
+        }
+
+        public function setNumeroSeguridadSocial($numeroSeguridadSocial)
+        {
+                $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+
+                return $this;
+        }
+
+        public abstract function ingresos();
+
+        public function mostrar()
+        {
+            echo "Está empleado ". ($this->nombre).($this->apellido) ." con el NSS: ". ($this->numeroSeguridadSocial).
+            "<br>Los ingresos ". ($this->ingresos());
+        }
+    }
